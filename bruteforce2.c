9600d061ba670f35207fede2819c23f4156bde20
Andrea Lam <lam20102@uvg.edu.gt>
2023-10-11 22:50:21 -0600

    funcional paralelo que lee un texto

--- bruteforce2.c
+++ bruteforce2.c
@@ -2,70 +2,158 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <unistd.h>
 #include <openssl/des.h>
 
-void decrypt(DES_key_schedule key, unsigned char *ciph) {
-    DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &key, DES_DECRYPT);
+void decrypt(long key, char *ciph, int len){
+  DES_cblock key_block;
+  DES_key_schedule schedule;
+  
+  memset(key_block, 0, sizeof(DES_cblock));
+  memcpy(key_block, &key, sizeof(long));  // copy the key into the key_block
+  
+  DES_set_key_unchecked(&key_block, &schedule);
+  DES_ecb_encrypt((DES_cblock *)ciph, (DES_cblock *)ciph, &schedule, DES_DECRYPT);
 }
 
-void encrypt(DES_key_schedule key, unsigned char *ciph) {
-    DES_ecb_encrypt((const_DES_cblock *)ciph, (DES_cblock *)ciph, &key, DES_ENCRYPT);
+void encrypt(long key, char *ciph, int len){
+  DES_key_schedule schedule;
+  DES_cblock key_block;
+  memcpy(key_block, &key, sizeof(key));  // copy the key into the key_block
+  DES_set_key_unchecked(&key_block, &schedule);
+  DES_ecb_encrypt((DES_cblock *)ciph, (DES_cblock *)ciph, &schedule, DES_ENCRYPT);
 }
 
 char search[] = " the ";
 
-int tryKey(DES_key_schedule key, unsigned char *ciph) {
-    unsigned char temp[8];
-    memcpy(temp, ciph, 8);
-    decrypt(key, temp);
-    return strstr((char *)temp, search) != NULL;
+int tryKey(long key, char *ciph, int len){
+  char temp[len+1];
+  memcpy(temp, ciph, len);
+  temp[len]=0;
+  decrypt(key, temp, len);
+  return strstr((char *)temp, search) != NULL;
 }
 
-unsigned char cipher[] = {108, 245, 65, 63, 125, 200, 150, 66};
+int loadTextFromFile(const char *filename, char **text, int *length) {
+  FILE *file = fopen(filename, "r");
+  if (file == NULL) {
+    perror("Error opening file");
+    return 0;
+  }
 
-int main(int argc, char *argv[]) {
-    int N, id;
-    DES_key_schedule des_key;
-    MPI_Status st;
-    MPI_Request req;
-    int flag;
-    MPI_Comm comm = MPI_COMM_WORLD;
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(comm, &N);
-    MPI_Comm_rank(comm, &id);
+  fseek(file, 0, SEEK_END);
+  *length = ftell(file);
+  fseek(file, 0, SEEK_SET);
 
-    DES_cblock key;
-    DES_string_to_key("my_password", &key);
-    DES_set_key_checked(&key, &des_key);
+  *text = (char *)malloc(*length);
+  if (*text == NULL) {
+    perror("Memory allocation error");
+    fclose(file);
+    return 0;
+  }
 
-    unsigned long long range = 0xFFFFFFFFFFFFFFFF; // Espacio de búsqueda completo
-    unsigned long long range_per_node = range / N;
-    unsigned long long mylower = id * range_per_node;
-    unsigned long long myupper = (id + 1) * range_per_node - 1;
+  fread(*text, 1, *length, file);
+  fclose(file);
 
-    // Sincronización antes de iniciar la búsqueda (elimina barreras innecesarias)
-    MPI_Barrier(comm);
+  return 1;
+}
 
-    unsigned long long found = 0;
-    int search_finished = 0; // Variable de bandera para controlar la terminación del bucle de búsqueda
+int saveTextToFile(const char *filename, char *text, int length) {
+  FILE *file = fopen(filename, "w");
+  if (file == NULL) {
+    perror("Error opening file");
+    return 0;
+  }
 
-    for (unsigned long long i = mylower; i < myupper && !search_finished; ++i) {
-        if (tryKey(des_key, cipher)) {
-            found = i;
-            search_finished = 1; // Se encontró la clave, establecer la bandera en 1
-            // Sincronización antes de compartir el resultado (elimina barreras innecesarias)
-            MPI_Barrier(comm);
-            for (int node = 0; node < N; node++) {
-                MPI_Send(&found, 1, MPI_UNSIGNED_LONG_LONG, node, 0, MPI_COMM_WORLD);
-            }
-        }
+  fwrite(text, 1, length, file);
+  fclose(file);
+
+  return 1;
+}
+
+unsigned char cipher[] = {108, 245, 65, 63, 125, 200, 150, 66, 17, 170, 207, 170, 34, 31, 70, 215, 0};
+
+int main(int argc, char *argv[]){
+  int N, id;
+  long upper = (1L << 56); // upper bound DES keys 2^56
+  long mylower, myupper;
+  MPI_Status st;
+  MPI_Request req;
+  int flag;
+  int ciphlen = strlen(cipher);
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(comm, &N);
+  MPI_Comm_rank(comm, &id);
+
+  int range_per_node = upper / N;
+  mylower = range_per_node * id;
+  myupper = range_per_node * (id+1) - 1;
+  if (id == N - 1) {
+    // Compensar residuo
+    myupper = upper;
+  }
+
+  char *text; // Text to encrypt and decrypt
+  int textLength;
+
+  if (id == 0) {
+    // Load text from a file (e.g., "input.txt")
+    if (!loadTextFromFile("input.txt", &text, &textLength)) {
+      MPI_Finalize();
+      return 1;
     }
 
-    if (id == 0) {
-        MPI_Wait(&req, &st);
-        decrypt(des_key, cipher);
-        printf("%llu %s\n", found, cipher);
-    }
+    // Encrypt the loaded text using a key (e.g., 42)
+    long encryptionKey = 42;
+    encrypt(encryptionKey, text, textLength);
 
-    MPI_Finalize();
+    // Save the encrypted text to a file (e.g., "encrypted.txt")
+    if (!saveTextToFile("encrypted.txt", text, textLength)) {
+      free(text);
+      MPI_Finalize();
+      return 1;
+    }
+  }
+
+  // Broadcast the encrypted text to all nodes
+  MPI_Bcast(&textLength, 1, MPI_INT, 0, comm);
+  if (id != 0) {
+    text = (char *)malloc(textLength);
+  }
+  MPI_Bcast(text, textLength, MPI_CHAR, 0, comm);
+
+  long found = 0;
+
+  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+
+  for (int i = mylower; i < myupper && (found == 0); ++i) {
+    if (tryKey(i, text, textLength)) {
+      found = i;
+      for (int node = 0; node < N; node++) {
+        MPI_Send(&found, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
+      }
+      break;
+    }
+  }
+
+  if (id == 0) {
+    MPI_Wait(&req, &st);
+    decrypt(found, text, textLength);
+
+    // Print the decrypted text and the encryption key
+    printf("%li %s\n", found, text);
+
+    // Save the decrypted text to a file (e.g., "decrypted.txt")
+    if (!saveTextToFile("decrypted.txt", text, textLength)) {
+      free(text);
+      MPI_Finalize();
+      return 1;
+    }
+  }
+
+  free(text);
+
+  MPI_Finalize();
 }
