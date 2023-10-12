e0980e78a1849ee7818b60d73a7fabce0481ea2e
Andrea Lam <lam20102@uvg.edu.gt>
2023-10-11 22:58:41 -0600

    codigo comentado

--- bruteforce2.c
+++ bruteforce2.c
@@ -8,19 +8,27 @@
 void decrypt(long key, char *ciph, int len){
   DES_cblock key_block;
   DES_key_schedule schedule;
-  
+
+  // Inicializa el bloque de clave DES con ceros
   memset(key_block, 0, sizeof(DES_cblock));
-  memcpy(key_block, &key, sizeof(long));  // copy the key into the key_block
+  // Copia la clave proporcionada en el bloque de clave
+  memcpy(key_block, &key, sizeof(long));
   
+  // Crea un horario de clave DES basado en la clave
   DES_set_key_unchecked(&key_block, &schedule);
+  // Realiza una desencriptación DES en el texto cifrado
   DES_ecb_encrypt((DES_cblock *)ciph, (DES_cblock *)ciph, &schedule, DES_DECRYPT);
 }
 
 void encrypt(long key, char *ciph, int len){
   DES_key_schedule schedule;
   DES_cblock key_block;
-  memcpy(key_block, &key, sizeof(key));  // copy the key into the key_block
+
+  // Copia la clave proporcionada en el bloque de clave
+  memcpy(key_block, &key, sizeof(key));
+  // Crea un horario de clave DES basado en la clave
   DES_set_key_unchecked(&key_block, &schedule);
+  // Realiza una encriptación DES en el texto
   DES_ecb_encrypt((DES_cblock *)ciph, (DES_cblock *)ciph, &schedule, DES_ENCRYPT);
 }
 
@@ -32,6 +40,7 @@ int tryKey(long key, char *ciph, int len){
   temp[len]=0;
   decrypt(key, temp, len);
   
+  // Busca la cadena " the " en el texto desencriptado
   if (strstr((char *)temp, search) != NULL) {
     printf("Clave encontrada: %li\n", key);
     return 1;
@@ -40,25 +49,27 @@ int tryKey(long key, char *ciph, int len){
   return 0;
 }
 
-
 int loadTextFromFile(const char *filename, char **text, int *length) {
   FILE *file = fopen(filename, "r");
   if (file == NULL) {
-    perror("Error opening file");
+    perror("Error al abrir el archivo");
     return 0;
   }
 
+  // Determina la longitud del archivo
   fseek(file, 0, SEEK_END);
   *length = ftell(file);
   fseek(file, 0, SEEK_SET);
 
+  // Asigna memoria para el texto basado en la longitud del archivo
   *text = (char *)malloc(*length);
   if (*text == NULL) {
-    perror("Memory allocation error");
+    perror("Error de asignación de memoria");
     fclose(file);
     return 0;
   }
 
+  // Lee el contenido del archivo en el texto
   fread(*text, 1, *length, file);
   fclose(file);
 
@@ -68,10 +79,11 @@ int loadTextFromFile(const char *filename, char **text, int *length) {
 int saveTextToFile(const char *filename, char *text, int length) {
   FILE *file = fopen(filename, "w");
   if (file == NULL) {
-    perror("Error opening file");
+    perror("Error al abrir el archivo");
     return 0;
   }
 
+  // Escribe el contenido del texto en el archivo
   fwrite(text, 1, length, file);
   fclose(file);
 
@@ -82,7 +94,7 @@ unsigned char cipher[] = {108, 245, 65, 63, 125, 200, 150, 66, 17, 170, 207, 170
 
 int main(int argc, char *argv[]){
   int N, id;
-  long upper = (1L << 56); // upper bound DES keys 2^56
+  long upper = (1L << 56); // Límite superior para claves DES: 2^56
   long mylower, myupper;
   MPI_Status st;
   MPI_Request req;
@@ -94,29 +106,30 @@ int main(int argc, char *argv[]){
   MPI_Comm_size(comm, &N);
   MPI_Comm_rank(comm, &id);
 
+  // Calcula el rango de claves que cada proceso MPI debe buscar
   int range_per_node = upper / N;
   mylower = range_per_node * id;
   myupper = range_per_node * (id+1) - 1;
   if (id == N - 1) {
-    // Compensar residuo
+    // Compensar el residuo
     myupper = upper;
   }
 
-  char *text; // Text to encrypt and decrypt
+  char *text; // Texto para encriptar y desencriptar
   int textLength;
 
   if (id == 0) {
-    // Load text from a file (e.g., "input.txt")
+    // Carga el texto desde un archivo (por ejemplo, "input.txt")
     if (!loadTextFromFile("input.txt", &text, &textLength)) {
       MPI_Finalize();
       return 1;
     }
 
-    // Encrypt the loaded text using a key (e.g., 42)
+    // Encripta el texto cargado usando una clave (por ejemplo, 42)
     long encryptionKey = 42;
     encrypt(encryptionKey, text, textLength);
 
-    // Save the encrypted text to a file (e.g., "encrypted.txt")
+    // Guarda el texto encriptado en un archivo (por ejemplo, "encrypted.txt")
     if (!saveTextToFile("encrypted.txt", text, textLength)) {
       free(text);
       MPI_Finalize();
@@ -124,7 +137,7 @@ int main(int argc, char *argv[]){
     }
   }
 
-  // Broadcast the encrypted text to all nodes
+  // Difunde el texto encriptado a todos los nodos
   MPI_Bcast(&textLength, 1, MPI_INT, 0, comm);
   if (id != 0) {
     text = (char *)malloc(textLength);
@@ -149,10 +162,10 @@ int main(int argc, char *argv[]){
     MPI_Wait(&req, &st);
     decrypt(found, text, textLength);
 
-    // Print the decrypted text and the encryption key
+    // Imprime el texto desencriptado y la clave de encriptación
     printf("%li %s\n", found, text);
 
-    // Save the decrypted text to a file (e.g., "decrypted.txt")
+    // Guarda el texto desencriptado en un archivo (por ejemplo, "decrypted.txt")
     if (!saveTextToFile("decrypted.txt", text, textLength)) {
       free(text);
       MPI_Finalize();
